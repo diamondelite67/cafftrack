43b9e107cccedddbef402fef7d39547f49dad225
diamondelite67 <eostovic@gmail.com>
2026-08-25 18:10:22 -0400

    Implement service worker for caching assets

---
+++ service-worker.js
@@ -0,0 +1,80 @@
+const CACHE_NAME = "cafftrack-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+
+self.addEventListener("install", event => {
+
+    self.skipWaiting();
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache =>
+                cache.addAll(FILES_TO_CACHE)
+            )
+    );
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key =>
+                        key !== CACHE_NAME
+                    )
+                    .map(key =>
+                        caches.delete(key)
+                    )
+            )
+        )
+
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+
+        fetch(event.request)
+            .then(response => {
+
+                const copy =
+                    response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache =>
+                        cache.put(
+                            event.request,
+                            copy
+                        )
+                    );
+
+                return response;
+
+            })
+            .catch(() =>
+                caches.match(event.request)
+            )
+
+    );
+
+});
