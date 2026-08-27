e01cba88f714849948aa6b25d7d05cf35e4f50e0
Evan <evan@Evans-MacBook-Neo.local>
2026-08-27 18:30:03 -0400

    Release CaffTrack v1.1.2

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = "caffTrackData";
-const APP_VERSION = "1.1.1";
+const APP_VERSION = "1.1.2";
 
 let data = loadData();
 let devDate = null;
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "cafftrack-v1.1.1";
+const CACHE_NAME = "cafftrack-v1.1.2";
 
 const FILES_TO_CACHE = [
     "./",
